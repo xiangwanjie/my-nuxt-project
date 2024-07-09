82aad0007ba3f804716a9f0b2a500482a0c1fac7
v_xiangwanjie <xiang.wanjie@redstarclouds.com>
2024-07-09 16:17:57 +0800

    feat: 详情返回上一页

--- pages/list/detail.vue
+++ pages/list/detail.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <p>详情页 - {{ $route.query.name ?? "" }}</p>
+    <NButton @click="router.back()">返回上一页</NButton>
     <nuxt-link to="/">返回首页</nuxt-link>
   </div>
 </template>
 
 <script setup lang="ts">
+import { NButton } from "naive-ui";
+import { useRouter } from "vue-router";
+const router = useRouter()
 </script>
